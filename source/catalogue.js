242d57337da5f83cf94771a75181f02bd42be757
Julian Webb <git@julianwebb.ca>
2021-11-28 16:14:31 -0800

    Cleanup: Moved Types to own file for reuse

--- source/catalogue.js
+++ source/catalogue.js
@@ -1,23 +1,7 @@
 const Filesystem = require('fs');
 const Path = require('path');
 
-const Types = {
-	TextFile: "0",
-	Directory: "1",
-	CSOPhoneBook: "2",
-	ErrorCondition: "3",
-	BinHexFile: "4",
-	DOSFile: "5",
-	UuencodedFile: "6",
-	IndexServer: "7",
-	TelnetSession: "8",
-	BinaryFile: "9",
-	RedundantServer: "+",
-	GIFImage: "g",
-	Image: "I",
-	TN3270Session: "T",
-	Information: "i"
-}
+const Types = require('./types');
 
 const TAB = "	";
 const NEWLINE = "\r\n";
---
+++ source/types.js
@@ -0,0 +1,19 @@
+const Types = {
+	TextFile: "0",
+	Directory: "1",
+	CSOPhoneBook: "2",
+	ErrorCondition: "3",
+	BinHexFile: "4",
+	DOSFile: "5",
+	UuencodedFile: "6",
+	IndexServer: "7",
+	TelnetSession: "8",
+	BinaryFile: "9",
+	RedundantServer: "+",
+	GIFImage: "g",
+	Image: "I",
+	TN3270Session: "T",
+	Information: "i"
+}
+
+module.exports = Types;
