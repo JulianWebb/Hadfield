4f815a2e04fa7b7178e97945a34f862d60df2c9a
Julian Webb <git@julianwebb.ca>
2021-11-28 16:46:26 -0800

    Add: Base Logic for Gopher

---
+++ source/gopher.js
@@ -0,0 +1,67 @@
+const Filesystem = require('fs');
+const Path = require('path');
+const Server = require('./server');
+const Catalogue = require('./catalogue');
+const Types = require('./types');
+
+async function GopherConstructor(configuration) {
+	const catalogue = await Catalogue({
+		documentRoot: configuration.documentRoot,
+		host: configuration.host,
+		port: configuration.port
+	});
+
+	this.server = await Server({
+		host: configuration.host,
+		port: configuration.port,
+		assetRoot: configuration.assetRoot
+	});
+	const gopher = new Gopher(configuration, catalogue, server);
+
+	return gopher;
+}
+
+class Gopher {
+	constructor (configuration, catalogue, server) {
+		this.configuration = configuration;
+		this.catalogue = catalogue;
+		this.server = server;
+		this.server.on('message', this.serve.bind(this));
+	}
+	
+	serve(message, socket) {
+		if (message.startsWith("\u0009")) {
+			this.reply(`3GopherPlus not current implemented	Err	${this.host}	${this.port}`, socket);
+			return void 0;
+		}
+
+		const { type, item } = this.catalogue.getItem(message);
+		switch (type) {
+			case "failure":
+				this.reply(`3${item.description}	Err	${this.host}	${this.port}\r\n`, socket);
+				break;
+			case "menu":
+				this.reply(item.display, socket);
+				break;
+			case Types.TextFile:
+				let text = Filesystem.readFileSync(Path.join(this.configuration.assetRoot, item.properties.file), "utf-8");
+				this.reply(text + "\r\n", socket);
+				break;
+			default:
+				this.reply(item.entry, socket);
+				break;
+		}
+	}
+
+	reply(message, socket) {
+		socket.write(message).then(() => {
+			socket.end('.');
+		})
+	}
+
+	async listen() {
+		return await this.server.listen();
+	}
+}
+
+module.exports = GopherConstructor;
